1f00320597bae8110c5de6a7e87bb6663f8aecba
Jeffrey Alahira <alahirajeffrey@gmail.com>
2024-12-06 23:36:47 +0100

    commit

--- src/demo.py
+++ src/demo.py
@@ -1,5 +1,15 @@
 import pandas as pd
 import os
+from dotenv import load_dotenv
+import lyricsgenius
+
+#  load environment variables
+load_dotenv()
+
+## init lyrics genuis 
+genius = lyricsgenius.Genius(os.getenv("GENIUS_ACCESS_TOKEN"))
+# genius = Genius()
+
 
 ## get project root directory
 root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
@@ -14,6 +24,12 @@ first_streaming_history = pd.read_json(first_file_path)
 second_streaming_history = pd.read_json(second_file_path)
 third_streaming_history= pd.read_json(third_file_path)
 
-##  concat all streaming history
+## concat all streaming history
 combined_streaming_history = pd.concat([first_streaming_history, second_streaming_history, third_streaming_history], ignore_index=True)
-print(combined_streaming_history.head())
+
+## get required data
+required_streaming_data = combined_streaming_history[
+    ["master_metadata_track_name", 
+     "master_metadata_album_artist_name", 
+     "master_metadata_album_album_name"]]
+print(required_streaming_data.head())
